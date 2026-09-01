5d12f1202c7358164e659405509880909f0c98c4
Smart-imran <rst.imran4@gmail.com>
2026-09-02 00:56:19 +0600

    question 2 solve

---
+++ question2.js
@@ -0,0 +1,20 @@
+
+function getDayType(day) {
+  const normalizedDay = day.toLowerCase();
+
+  switch (normalizedDay) {
+    case "friday":
+    case "saturday":
+      return "Weekend";
+    case "sunday":
+    case "monday":
+    case "tuesday":
+    case "wednesday":
+    case "thursday":
+      return "Working Day";
+    default:
+      return "Invalid Day";
+  }
+}
+
+console.log(getDayType("MONDAY"))
